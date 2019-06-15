f13614697bf018cf97d0e3becfab165031f4c67c
YevhDr <evgesha.drozdova@gmail.com>
2019-06-15 21:44:33 +0300

    init

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var $form = $('form#test-form'),
+    url = 'https://script.google.com/macros/s/AKfycbzyBXD8-1tK1igw9cN31S1ak9Ktwip00HOg0euP1SjKnwHpkyyz/exec';
+
+$('#submit-form').on('click', function(e) {
+    e.preventDefault();
+    var jqxhr = $.ajax({
+        url: url,
+        method: "GET",
+        dataType: "json",
+        data: $form.serializeObject()
+    }).success(
+        // do something
+    );
+})
