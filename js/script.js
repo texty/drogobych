ac8e56e1f17ed808088cdd7add6123522d7f6918
YevhDr <evgesha.drozdova@gmail.com>
2019-06-25 13:34:31 +0300

    init

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+//відповіді форми збираються тут: https://docs.google.com/spreadsheets/d/1MZUQuWk3SkhWqVoakE22nfi-_DGfqBvUPsR9qiEscsk/edit#gid=0
+
 var $form = $('form#test-form'),
     url = 'https://script.google.com/macros/s/AKfycbzyBXD8-1tK1igw9cN31S1ak9Ktwip00HOg0euP1SjKnwHpkyyz/exec';
 
