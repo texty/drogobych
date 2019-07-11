e78382c8759ab45b148265f3d58b978abd43fd48
YevhDr <evgesha.drozdova@gmail.com>
2019-07-11 19:45:50 +0300

    розібралась з валідацієї форми

--- js/script.js
+++ js/script.js
@@ -33,7 +33,7 @@ $('#submit-form').on('click', function(e) {
 
 
 document.addEventListener("DOMContentLoaded", function() {
-    var elements = document.getElementsByTagName("INPUT");
+    var elements = document.getElementsByClassName("required");
     for (var i = 0; i < elements.length; i++) {
         elements[i].oninvalid = function(e) {
             e.target.setCustomValidity("");
