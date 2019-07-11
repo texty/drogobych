e3d1e9af2a3fe25976db5053b1d901eb0142cf84
YevhDr <evgesha.drozdova@gmail.com>
2019-07-11 19:44:12 +0300

    розібралась з валідацієї форми

--- js/script.js
+++ js/script.js
@@ -3,14 +3,49 @@
 var $form = $('form#test-form'),
     url = 'https://script.google.com/macros/s/AKfycbzyBXD8-1tK1igw9cN31S1ak9Ktwip00HOg0euP1SjKnwHpkyyz/exec';
 
+
 $('#submit-form').on('click', function(e) {
-    e.preventDefault();
-    var jqxhr = $.ajax({
-        url: url,
-        method: "GET",
-        dataType: "json",
-        data: $form.serializeObject()
-    }).success(
-        console.log("done")
-    );
-})
+        //e.preventDefault();
+        var empty = false;
+        $('.required').each(function(){
+            if($(this).val() === ''){
+                console.log($(this).val());
+                empty = false;
+            } else {
+                console.log($(this).val());
+                empty = true;
+            }
+        });
+
+        console.log(empty);
+    if(empty === true){
+        var jqxhr = $.ajax({
+            url: url,
+            method: "GET",
+            dataType: "json",
+            data: $form.serializeObject()
+        }).success(
+            console.log("done")
+        );
+    }
+
+});
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    var elements = document.getElementsByTagName("INPUT");
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].oninvalid = function(e) {
+            e.target.setCustomValidity("");
+            if (!e.target.validity.valid) {
+                e.target.setCustomValidity("Надайте необхідну інформацію");
+            }
+        };
+
+        elements[i].oninput = function(e) {
+            e.target.setCustomValidity("");
+
+        };
+    }
+});
+
